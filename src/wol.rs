a608217e5bb4f4c48aef6894db7c2e973c823196
Darwin Darakananda <darwin@cu.co.th>
2024-10-27 14:47:12 +0700

    test: refactor and test WOL packet construction

--- src/wol.rs
+++ src/wol.rs
@@ -1,5 +1,6 @@
+use pnet::datalink::NetworkInterface;
 use pnet::datalink::{self, Channel::Ethernet};
-use pnet::packet::ethernet::{EtherTypes, MutableEthernetPacket};
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
 use pnet::packet::{MutablePacket, Packet};
 use pnet::util::MacAddr;
 
@@ -61,7 +62,11 @@ fn vlan_to_bytes(vlan: u16) -> Vec<u8> {
     vlan_tag.to_vec()
 }
 
-pub fn send_wol_packet(maybe_mac: &str, interface_name: &str, vlan_id: Option<u16>) -> Result<()> {
+pub fn build_wol_packet(
+    maybe_mac: &str,
+    interface_name: &str,
+    vlan_id: Option<u16>,
+) -> Result<(Vec<u8>, NetworkInterface)> {
     let mac = maybe_mac
         .parse::<MacAddr>()
         .map_err(|_| WOLError::InvalidMAC(maybe_mac.to_string()))?;
@@ -72,17 +77,6 @@ pub fn send_wol_packet(maybe_mac: &str, interface_name: &str, vlan_id: Option<u1
         .into_iter()
         .find(|iface| iface.name == interface_name)
         .ok_or_else(|| WOLError::InterfaceNotFound(interface_name.to_string()))?;
-
-    let mut tx = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(tx, _)) => tx,
-        Ok(_) => {
-            return Err(WOLError::NetworkError(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "unhandled channel type for this interface",
-            )))
-        }
-        Err(e) => return Err(WOLError::NetworkError(e)),
-    };
 
     let payload_size = if vlan_id.is_some() {
         SIZE_VLAN_TAG + SIZE_VLAN_ETHERTYPE
@@ -125,14 +119,126 @@ pub fn send_wol_packet(maybe_mac: &str, interface_name: &str, vlan_id: Option<u1
 
     packet.payload_mut()[payload_offset..].copy_from_slice(&wol_packet);
 
+    Ok((buffer, interface))
+}
+
+pub fn send_wol_packet(maybe_mac: &str, interface_name: &str, vlan_id: Option<u16>) -> Result<()> {
+    let (packet_buffer, interface) = build_wol_packet(maybe_mac, interface_name, vlan_id)?;
+
+    let packet = EthernetPacket::new(&packet_buffer)
+        .expect("`packet_buffer` was created by a `MutableEthernetPacket`, should not error here");
+
+    let mut tx = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, _)) => tx,
+        Ok(_) => {
+            return Err(WOLError::NetworkError(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "unhandled channel type for this interface",
+            )))
+        }
+        Err(e) => return Err(WOLError::NetworkError(e)),
+    };
+
     tx.send_to(packet.packet(), None).ok_or_else(|| {
         std::io::Error::new(std::io::ErrorKind::Other, "failed to send WOL packet")
     })??;
 
-    //println!(
-    //    "WOL packet sent successfully over interface: {}",
-    //    interface_name
-    //);
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_invalid_mac_address() {
+        let invalid_mac = "random MAC";
+        let interface_name = "eth0";
+
+        let result = send_wol_packet(invalid_mac, interface_name, None);
+        assert!(
+            matches!(result, Err(WOLError::InvalidMAC(_))),
+            "Expected InvalidMAC error."
+        );
+    }
+
+    #[test]
+    fn test_missing_network_interface() {
+        let mac = "00:11:22:33:44:55";
+        let non_existent_interface = "nonexistent_iface";
+
+        let result = send_wol_packet(mac, non_existent_interface, None);
+        assert!(
+            matches!(result, Err(WOLError::InterfaceNotFound(_))),
+            "Expected InterfaceNotFound error."
+        );
+    }
+
+    #[test]
+    fn test_ethernet_packet() {
+        let maybe_mac = "01:23:45:67:89:AB";
+        let mac = maybe_mac.parse::<MacAddr>().unwrap();
+        let dest_mac = MacAddr::broadcast();
+        //let vlan_id = Some(100);
+
+        let interface = datalink::interfaces()
+            .into_iter()
+            .find(|iface| iface.mac.is_some())
+            .expect("cannot find an interface with a MAC address for testing");
+
+        let payload_size = SIZE_WOL_PAYLOAD;
+        let packet_size = SIZE_DST_MAC + SIZE_SRC_MAC + SIZE_ETHERTYPE + payload_size;
+
+        let (buffer, _interface) = build_wol_packet(maybe_mac, &interface.name, None)
+            .expect("failed to build test packet");
+
+        assert_eq!(packet_size, buffer.len());
+
+        // L2 broadcast address
+        assert_eq!(dest_mac.octets(), buffer[..6]);
+        // Source MAC
+        assert_eq!(interface.mac.unwrap().octets(), buffer[6..12]);
+        // WOL Ethertype
+        assert_eq!(vec![0x08, 0x42], buffer[12..14]);
+        // WOL packet prefix
+        assert_eq!(vec![0xFF; 6], buffer[14..20]);
+        // WOL target MAC x 16
+        assert_eq!(mac.octets().repeat(16), buffer[20..]);
+    }
+
+    #[test]
+    fn test_ethernet_packet_with_vlan() {
+        let maybe_mac = "01:23:45:67:89:AB";
+        let mac = maybe_mac.parse::<MacAddr>().unwrap();
+        let dest_mac = MacAddr::broadcast();
+        let vlan_id = Some(0x0101);
+
+        let interface = datalink::interfaces()
+            .into_iter()
+            .find(|iface| iface.mac.is_some())
+            .expect("cannot find an interface with a MAC address for testing");
+
+        let payload_size = SIZE_VLAN_TAG + SIZE_VLAN_ETHERTYPE + SIZE_WOL_PAYLOAD;
+        let packet_size = SIZE_DST_MAC + SIZE_SRC_MAC + SIZE_ETHERTYPE + payload_size;
+
+        let (buffer, _interface) = build_wol_packet(maybe_mac, &interface.name, vlan_id)
+            .expect("failed to build test packet");
+
+        assert_eq!(packet_size, buffer.len());
+
+        // L2 broadcast address
+        assert_eq!(dest_mac.octets(), buffer[..6]);
+        // Source MAC
+        assert_eq!(interface.mac.unwrap().octets(), buffer[6..12]);
+        // VLAN Ethertype
+        assert_eq!(vec![0x81, 0x00], buffer[12..14]);
+        // VLAN Tag
+        assert_eq!(vec![0x01, 0x01], buffer[14..16]);
+        // WOL Ethertype
+        assert_eq!(vec![0x08, 0x42], buffer[16..18]);
+        // WOL packet prefix
+        assert_eq!(vec![0xFF; 6], buffer[18..24]);
+        // WOL target MAC x 16
+        assert_eq!(mac.octets().repeat(16), buffer[24..]);
+    }
 }
